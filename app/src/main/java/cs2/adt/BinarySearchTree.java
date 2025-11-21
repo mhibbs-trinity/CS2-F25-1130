9865f1ae168514d5b9cf64777939486904d12520
Matt Hibbs <mhibbs@trinity.edu>
2025-11-21 12:18:01 -0600

    started binary search tree

---
+++ app/src/main/java/cs2/adt/BinarySearchTree.java
@@ -0,0 +1,58 @@
+package cs2.adt;
+
+public class BinarySearchTree<T extends Comparable<T>> {
+    private class Node {
+        public T data; public Node left; public Node right;
+        public Node(T d, Node l, Node r) {
+            data = d; left = l; right = r;
+        }
+        public boolean contains(T elem) {
+            if(data.compareTo(elem) == 0) return true;
+            if(data.compareTo(elem) < 0) {
+                if(right == null) return false;
+                return right.contains(elem);
+            } else {
+                if(left == null) return false;
+                return left.contains(elem);
+            }
+        }
+        public void insert(T elem) {
+            if(data.compareTo(elem) < 0) {
+                if(right == null) right = new Node(elem, null, null);
+                else right.insert(elem);
+            } else {
+                if(left == null) left = new Node(elem, null, null);
+                left.insert(elem);
+            }
+        }
+    }
+
+    private Node root;
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public boolean isEmpty() { return root == null; }
+    public void insert(T elem) {
+        if(root == null) root = new Node(elem, null, null);
+        root.insert(elem);
+    }
+    public boolean contains(T elem) {
+        if(root == null) return false;
+        return root.contains(elem);
+        /*
+        Node current = root;
+        while(current != null) {
+            if(current.data.compareTo(elem) == 0) return true;
+            if(current.data.compareTo(elem) < 0) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
+        return false;
+        */
+    }
+
+    
+}
